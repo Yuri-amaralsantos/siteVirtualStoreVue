98e8973c8755a1eea5d9399c66ee8ff355d4485d
yuri amaral santos <113852509+Yuri-amaralsantos@users.noreply.github.com>
2025-01-15 13:52:38 -0300

    vercel

--- Site/vite.config.js
+++ Site/vite.config.js
@@ -3,5 +3,8 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/siteVirtualStoreVue/",
+  base: "/", // Use '/' for the root or '/subpath/' for a subdirectory
+  build: {
+    outDir: "dist",
+  },
 });
